909a04346561cf63a204ed9c44b4de32a53e93e6
YevinDong <dongyewen@hotmail.com>
2022-11-01 16:21:01 +0800

    fix: release

--- release.js
+++ release.js
@@ -30,7 +30,7 @@ inquirer
   ])
   .then((answer) => {
     if(exec(`npm version ${answer.selected}`).code !== 0) return
-    if(exec(`npm publish`).code !== 0) return
+    if(exec(`npm publish --registry https://registry.npmjs.org`).code !== 0) return
     console.log(symbols.success, chalk.green(`版本更新为${require('./package.json').version}`));
     // exec('npm publish')
   });
